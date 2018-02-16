db9233dfa0488b6c0926a1e191e6146817c83f7d
Skand Gupta <32306644+skandg41@users.noreply.github.com>
2018-02-17 00:31:06 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.get('/', function (req, res) {
 });
 
 app.get('/article-one/',function(req,res){
-    res.send("Page one will be showed");
+
     res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
 });
 
