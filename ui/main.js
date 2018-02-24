805efcb8fc7d63e7e82a10ea05c2c1f4462a089f
Skand Gupta <32306644+skandg41@users.noreply.github.com>
2018-02-25 00:27:12 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,11 +1,21 @@
 //Counter Code
 var button=get.ElementById("counter");
-var counter=0;
+
 button.onclick =function(){
     
+    var request =new XMLHttpRequest();
     
-    counter = counter +1;
-    var span=document.getElementById("count");
-    span.innerHTML=counter.toString();
+    request.onreadyststechange=function(){
+        if (request.readystate==XMLHttpRequest.DONE){
+            if(request.status==200){
+                var counter = request.responseText;
+                var span=document.getElementById("count");
+                span.innerHTML=counter.toString();
+
+            }
+        }
+        
+    }
+    
     
 };
