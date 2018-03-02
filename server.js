b8f8f8a6d05173a82f10b7e522ac1ae13058d2d7
Skand Gupta <32306644+skandg41@users.noreply.github.com>
2018-03-02 23:59:35 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -57,7 +57,7 @@ app.get('submit-name',function(req,res){
 
 app.get('/articles/:articleName',function(req,res){
    
-   pool.query("SELECT * from article where title= "+req.params.articleName , function(err,result){
+   pool.query("SELECT * from article where title='"+req.params.articleName +"'" , function(err,result){
        if(err)
        {
            res.status(500).send(err.toString());
