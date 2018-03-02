0b822b6cb7a133ac9ddb8de564d3f68adcee9f02
Skand Gupta <32306644+skandg41@users.noreply.github.com>
2018-03-02 22:54:10 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-var counter=0;
+
 
 var pool = new Pool(config);
 app.get('/test-db',function(req,res){
@@ -40,7 +40,7 @@ app.get('/test-db',function(req,res){
        
    });
 });
-
+var counter=0;
 app.get('/counter',function(req,res){
     counter =counter+1;
     res.send(counter.toString());
