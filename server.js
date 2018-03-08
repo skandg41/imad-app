10f8312b77a1755dbec3bca72f71d05a38ec35af
Skand Gupta <32306644+skandg41@users.noreply.github.com>
2018-03-08 23:14:06 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.get('/', function (req, res) {
 
 function hash (input,salt){
     //How do we create a hash
-    var hashed = crypto.pbkdf25Sync(input,salt,10000,512,'sha512');
+    var hashed = crypto.pbkdf2(input,salt,10000,512,'sha512');
     return hashed.toString('hex');
 }
 
