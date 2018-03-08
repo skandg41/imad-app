a60c74e306a1e72d50bddf6ada54474f26551901
Skand Gupta <32306644+skandg41@users.noreply.github.com>
2018-03-09 00:15:52 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ var config={
 
 var app = express();
 app.use(morgan('combined'));
-app.use(bosyParser.json());
+app.use(bodyParser.json());
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
