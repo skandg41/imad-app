71894c559f3be101514416186e46ef591d637d93
Skand Gupta <32306644+skandg41@users.noreply.github.com>
2018-03-08 23:58:32 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -58,7 +58,7 @@ app.post('/login',function(req,res){
    var password = req.body.password;
    
    var dbString = hash(password,salt);
-   pool.query('INSERT INTO "user" (username,password) VALUES($1,$2)',[username,dbString],function(err,res){
+   pool.query('insert into "user"(username,password) values ($1,$2)',[username,dbString],function(err,result){
     if(err)
   {
       res.status(500).send(err.toString());
