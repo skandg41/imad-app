a3ed82a69955d260eb9f3a5937fee32d5e52dcc4
Skand Gupta <32306644+skandg41@users.noreply.github.com>
2018-03-08 23:56:11 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -37,7 +37,7 @@ app.post('/create-user',function(req,res){
    var username = req.body.username;
    var password = req.body.password;
    
-   var salt = crypto.randomBytes(128).toStrig('hex');
+   var salt = crypto.randomBytes(128).toString('hex');
    var dbString = hash(password,salt);
    pool.query('INSERT INTO "user" (username,password) VALUES($1,$2)',[username,dbString],function(err,res){
     if(err)
@@ -50,6 +50,24 @@ app.post('/create-user',function(req,res){
   }     
    });
     
+});
+
+app.post('/login',function(req,res){
+   
+       var username = req.body.username;
+   var password = req.body.password;
+   
+   var dbString = hash(password,salt);
+   pool.query('INSERT INTO "user" (username,password) VALUES($1,$2)',[username,dbString],function(err,res){
+    if(err)
+  {
+      res.status(500).send(err.toString());
+  }
+  else
+  {
+      res.send('User successfullt created' + username);
+  }     
+   });
 });
 
 var pool = new Pool(config);
